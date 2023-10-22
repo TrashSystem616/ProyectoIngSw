267e9bb4fe2efdcacc00f5e9ec791e7850a68711
César Armando:) <128873992+TrashSystem616@users.noreply.github.com>
2023-10-22 15:04:59 -0600

    Login

--- ProyectoIngSw/Form1.Designer.vb
+++ ProyectoIngSw/Form1.Designer.vb
@@ -25,13 +25,19 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(156, 139)
+        Me.Button1.Location = New System.Drawing.Point(175, 177)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(130, 38)
+        Me.Button1.Size = New System.Drawing.Size(101, 33)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Verificar"
         Me.Button1.UseVisualStyleBackColor = True
@@ -40,31 +46,91 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(19, 70)
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(415, 21)
+        Me.Label1.Size = New System.Drawing.Size(108, 21)
         Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Bienvenido, por favor inicie con su contraseña"
+        Me.Label1.Text = "Bienvenido"
         '
         'TextBox1
         '
+        Me.TextBox1.Enabled = False
         Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(117, 107)
+        Me.TextBox1.Location = New System.Drawing.Point(149, 80)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(206, 26)
+        Me.TextBox1.Size = New System.Drawing.Size(150, 26)
         Me.TextBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label2.Location = New System.Drawing.Point(184, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 21)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Usuario:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(149, 135)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox2.Size = New System.Drawing.Size(150, 26)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label3.Location = New System.Drawing.Point(171, 111)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(110, 21)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Contraseña:"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 199)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(88, 27)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Conexion"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.ProyectoIngSw.My.Resources.Resources.LogoKFC
+        Me.PictureBox1.Location = New System.Drawing.Point(365, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(81, 89)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.IndianRed
+        Me.BackColor = System.Drawing.Color.DarkRed
         Me.ClientSize = New System.Drawing.Size(446, 238)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Inicio"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -73,4 +139,9 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- ProyectoIngSw/Form1.vb
+++ ProyectoIngSw/Form1.vb
@@ -2,14 +2,45 @@
 
 Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim conexion As SqlConnection = ObtenerConexion()
-        Try
+        Dim contraseña As String = TextBox2.Text
+        Dim usuario As String = ""
+
+        Using conexion As SqlConnection = ConexionBD.ObtenerConexion()
             conexion.Open()
-            MessageBox.Show("Conexión exitosa a la base de datos.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            Dim query As String = "SELECT COUNT(*) FROM Usuarios WHERE Contraseña = @Contraseña"
+            Using command As New SqlCommand(query, conexion)
+                command.Parameters.AddWithValue("@Contraseña", contraseña)
+
+                Dim count As Integer = CInt(command.ExecuteScalar())
+
+                If count > 0 Then
+                    ' Realizar una consulta adicional para obtener el nombre del usuario
+                    Dim queryNombre As String = "SELECT NombreUsuario FROM Usuarios WHERE Contraseña = @Contraseña"
+                    Using commandNombre As New SqlCommand(queryNombre, conexion)
+                        commandNombre.Parameters.AddWithValue("@Contraseña", contraseña)
+                        usuario = CStr(commandNombre.ExecuteScalar())
+                    End Using
+
+                    MessageBox.Show("¡Bienvenido, " & usuario & "!")
+                Else
+                    MessageBox.Show("Contraseña incorrecta. Inténtalo de nuevo.")
+                End If
+            End Using
+
+            conexion.Close() ' Cierra la conexión al finalizar
+        End Using
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Using conexion As SqlConnection = ConexionBD.ObtenerConexion()
+                conexion.Open()
+                MessageBox.Show("Conexión exitosa a la base de datos", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                ' Aquí puedes realizar otras operaciones de base de datos si es necesario
+            End Using
         Catch ex As Exception
-            MessageBox.Show("Error al conectar a la base de datos: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        Finally
-            conexion.Close()
+            MessageBox.Show("Error de conexión: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
     End Sub
 End Class
